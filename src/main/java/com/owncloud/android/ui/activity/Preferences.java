82d26468970f4123cadf7951d7eed6639c2f8a62
Bartosz Przybylski <bart.p.pl@gmail.com>
2016-06-12 15:34:55 +0200

    tabs to spaces

--- src/main/java/com/owncloud/android/ui/activity/Preferences.java
+++ src/main/java/com/owncloud/android/ui/activity/Preferences.java
@@ -200,8 +200,7 @@ public class Preferences extends PreferenceActivity
             }
         });
         mPrefTimeBetweenSynchronizations = findPreference("time_between_sync");
-		if (mPrefTimeBetweenSynchronizations != null) {
-
+        if (mPrefTimeBetweenSynchronizations != null) {
             mPrefTimeBetweenSynchronizations.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object newValueObject) {
@@ -215,8 +214,7 @@ public class Preferences extends PreferenceActivity
                     return false;
                 }
             });
-
-		}
+        }
 
         PreferenceCategory preferenceCategory = (PreferenceCategory) findPreference("more");
 
